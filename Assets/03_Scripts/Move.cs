b203ac73386345b10eeb270922c75e08134351f3
Luca Rava <luca.rava@stud.hslu.ch>
2020-02-01 04:35:37 +0100

    implement Movement of Player

---
+++ Assets/03_Scripts/Move.cs
@@ -0,0 +1,60 @@
+﻿using UnityEngine;
+
+public class Move : MonoBehaviour
+{
+
+    [SerializeField]
+    private float speed = 5f;
+
+    private bool isMoveLeft;
+    private bool isMoveRight;
+
+    private MoveController moveController;
+    private Rigidbody2D rb;
+
+    private void Awake()
+    {
+        moveController = GetComponent<MoveController>();
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    void Start()
+    {
+        moveController.MoveLeft += OnMoveLeft;
+        moveController.MoveRight += OnMoveRight;
+    }
+
+    private void OnMoveLeft()
+    {
+            isMoveLeft = true;
+    }
+
+    private void OnMoveRight()
+    {
+            isMoveRight = true;
+    }
+
+    private void FixedUpdate()
+    {
+        var pos = Vector3.zero;
+
+        if (isMoveLeft)
+        {
+            rb.MovePosition(transform.position + Vector3.left * Time.fixedDeltaTime * speed);
+            isMoveLeft = false;
+        }
+
+        if (isMoveRight)
+        {
+            rb.MovePosition(transform.position + Vector3.right * Time.fixedDeltaTime * speed);
+            isMoveRight = false;
+        }
+            
+    }
+
+    private void OnDestroy()
+    {
+        moveController.MoveLeft -= OnMoveLeft;
+        moveController.MoveRight -= OnMoveRight;
+    }
+}
---
+++ Assets/03_Scripts/MoveController.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using UnityEngine;
+
+public class MoveController : MonoBehaviour
+{
+    public event Action MoveLeft = delegate { };
+    public event Action MoveRight = delegate { };
+
+    private void Update()
+    {
+        if (Input.GetKey(KeyCode.A))
+        {
+            MoveLeft();
+        }
+
+        if (Input.GetKey(KeyCode.D))
+        {
+            MoveRight();
+        }
+    }
+}
+
+
