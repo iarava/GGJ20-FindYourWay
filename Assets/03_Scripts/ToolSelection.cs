aebda3e9abf1abcbe65a75917f03f5c25ce76e7e
Luca Rava <luca.rava@stud.hslu.ch>
2020-02-01 15:29:20 +0100

    first implementation of ToolSelection

---
+++ Assets/03_Scripts/ToolSelection.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ToolSelection : MonoBehaviour
+{
+    [SerializeField] private GameObject prefabAxe;
+    [SerializeField] private GameObject prefabPick;
+    [SerializeField] private GameObject prefabRamp;
+    [SerializeField] private GameObject prefabBridge;
+    
+    public void OnSelectionAxe()
+    {
+        Instantiate(prefabAxe);
+    }
+    
+    public void OnSelectionPick()
+    {
+        Instantiate(prefabPick);
+    }
+    
+    public void OnSelectionRamp()
+    {
+        Instantiate(prefabRamp);
+    }
+    
+    public void OnSelectionBridge()
+    {
+        Instantiate(prefabBridge);
+    }
+}
