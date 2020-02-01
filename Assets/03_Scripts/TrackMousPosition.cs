2ce2a8f685c314079510f26bc87838b02c6a36b0
Luca Rava <luca.rava@stud.hslu.ch>
2020-02-01 14:02:22 +0100

    the different Tools now moving when Instantiate

--- Assets/03_Scripts/TrackMousPosition.cs
+++ Assets/03_Scripts/TrackMousPosition.cs
@@ -1,9 +1,29 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEditor;
 using UnityEngine;
+using UnityEngine.UIElements;
+using Cursor = UnityEngine.Cursor;
 
 public class TrackMousPosition : MonoBehaviour
 {
-    
+    [SerializeField] private float distance = 10f;
+
+    private void Awake()
+    {
+        Cursor.visible = false;
+    }
+
+    private void Update()
+    {
+        var mousePosition = new Vector3(Input.mousePosition.x, Input.mousePosition.y, distance);
+        var objPosition = Camera.main.ScreenToWorldPoint(mousePosition);
+        transform.position = objPosition;
+    }
+
+    private void OnDestroy()
+    {
+        Cursor.visible = true;
+    }
 }
